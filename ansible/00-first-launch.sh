48704b59c88ce855d5f2916b158752b2cdbafc58
slawagn <slawagn@gmail.com>
2022-07-15 15:43:28 +0300

    initial commit

---
+++ ansible/00-first-launch.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i inventory -e @admin-pass --ask-vault-pass ./playbooks/00-first-launch.yml --ask-pass
---
+++ ansible/01-provision.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i inventory --ask-vault-pass -e @admin-pass ./playbooks/01-provision.yml
